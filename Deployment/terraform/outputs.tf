ab31c30ef26385cb41c2f7b6475217baeb16c5dc
WhiteTom <whitetom@arcor.de>
2022-01-01 22:55:32 +0100

    case sensitive

--- Deployment/terraform/outputs.tf
+++ Deployment/terraform/outputs.tf
@@ -3,5 +3,5 @@ output "manifest_path" {
 }
 
 output "app_service_name" {
-  value = jsondecode(azurerm_resource_group_template_deployment.icebreaker.output_content).appServiceName.Value
+  value = jsondecode(azurerm_resource_group_template_deployment.icebreaker.output_content).appServiceName.value
 }
