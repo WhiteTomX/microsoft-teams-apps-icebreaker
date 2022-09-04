e669ed75cf6c25885e6d7d2dcc92745e14d76f5c
WhiteTom <whitetom@arcor.de>
2022-09-04 21:08:20 +0200

    fix renaming

--- manifest.tf
+++ manifest.tf
@@ -3,7 +3,7 @@ resource "local_file" "icon_color" {
   filename       = "${path.module}/tmp/color.png"
 }
 
-resource "local_file" "outline" {
+resource "local_file" "icon_outline" {
   content_base64 = filebase64(var.icon_outline_path)
   filename       = "${path.module}/tmp/outline.png"
 }
@@ -60,6 +60,6 @@ data "archive_file" "app_package" {
   output_path = "${path.module}/manifest.zip"
   source_dir  = "${path.module}/tmp"
   depends_on = [
-    local_file.logo, local_file.manifest
+    local_file.icon_outline, local_file.icon_color
   ]
 }
